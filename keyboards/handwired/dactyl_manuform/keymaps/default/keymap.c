508c10f6e2c70dd01487342f2e8a4770e88705f5
John Petty <johnpetty@Johns-Mac-mini.local>
2018-07-05 23:38:40 -0400

    current working 5x6 dactyl manuform

--- keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
@@ -51,13 +51,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_BASE] = LAYOUT( \
-  KC_Q, KC_W, KC_E,    KC_R,    KC_T,                                       KC_Y, KC_U,    KC_I,    KC_O,   KC_P,    \
-  KC_A, KC_S, KC_D,    KC_F,    KC_G,                                       KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN, \
-  KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                       KC_N, KC_M,    KC_COMM, KC_DOT, KC_QUOT, \
+KC_ESC,    KC_1,    KC_2,    KC_3,   KC_4,   KC_5,                                   KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,
+  KC_GRV, KC_Q, KC_W, KC_E,    KC_R,    KC_T,                                       KC_Y, KC_U,    KC_I,    KC_O,   KC_P,  KC_EQL, \
+  KC_TAB, KC_A, KC_S, KC_D,    KC_F,    KC_G,                                       KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
+  KC_LSHIFT, KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                       KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSHIFT, \
               KC_LBRC, KC_RBRC,                                                   KC_MINS, KC_EQL,                   \
                                       SFT_ESC, CTL_BSPC,  ALT_SPC, SFT_ENT,                                          \
-                                      KC_TAB,  KC_HOME,   KC_END,  KC_DEL,                                           \
-                                      RAISE,   KC_GRV,    KC_LGUI, LOWER
+                                      KC_LCTL,  KC_LGUI,     KC_LGUI,  KC_RALT,                                           \
+                                      LOWER,   KC_GRV,    KC_DEL, RAISE
 ),
 
 /* Raise
@@ -82,14 +83,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  ____, ____,  KC_MU, ____,  ____,                          KC_VOLU, ____,    KC_UP,   ____,     KC_PGUP,   \
-  ____, KC_ML, KC_MD, KC_MR, ____,                          KC_MUTE, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDOWN, \
-  ____, ____,  ____,  ____,  ____,                          KC_VOLD, KC_SLSH, KC_BSLS, KC_QUES,  KC_PIPE,   \
+ ____, ____, ____, ____, ____, ____,                               ____, ____, ____, ____, ____, ____,
+  ____, ____,  KC_MU, ____,  ____, ____,                          KC_VOLU, ____,    KC_UP,   ____,     KC_PGUP, ____,  \
+  ____, KC_ML, KC_MD, KC_MR, ____, ____,                          KC_MUTE, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDOWN, ____,\
+  ____, ____,  ____,  ____,  ____, ____,                        KC_VOLD, KC_SLSH, KC_BSLS, KC_QUES,  KC_PIPE,  ____, \
         ____,  ____,                                                          KC_MB1,  KC_MB2,              \
                                    ____, ____,  ____, ____,                                                 \
                                    ____, ____,  ____, ____,                                                 \
                                    ____, ____,  ____, ____                                                  \
 ),
+
 /* Lower
  * ,----------------------------------,                             ,----------------------------------,
  * | F1   | F2   | F3   | F4   | F5   |                             |  F6  | F7   |  F8  |  F9  |  F10 |
@@ -112,14 +115,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_LOWER] = LAYOUT( \
-  KC_F1,   KC_F2,  KC_F3,   KC_F4,  KC_F5,                            KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  \
-  KC_1,    KC_2,   KC_3,    KC_4,   KC_5,                             KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    \
-  KC_EXLM, KC_AT,  KC_HASH, KC_DLR, KC_PERC,                          KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, \
+____,____,____,____,____,____,                                        ____,____,____,____,____,____,
+  KC_F1,   KC_F2,  KC_F3,   KC_F4,  KC_F5,____,                            KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10, ____, \
+  KC_1,    KC_2,   KC_3,    KC_4,   KC_5, ____,                            KC_6,    KC_7,     KC_8,    KC_9,    KC_0, ____,   \
+  KC_EXLM, KC_AT,  KC_HASH, KC_DLR, KC_PERC, ____,                         KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, ____,\
            KC_F11, KC_F12,                                                               ____,    ____,             \
                                              ____, ____,  ____, ____,                                               \
                                              ____, ____,  ____, ____,                                               \
                                              ____, ____,  ____, ____                                                \
 )
+
 };
 
 void persistent_default_layer_set(uint16_t default_layer) {
